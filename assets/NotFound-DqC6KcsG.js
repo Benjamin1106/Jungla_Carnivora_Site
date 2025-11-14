74b602cdf249912c53c840b4a8c91f47748597bd
Benja <benjamin.salas.escobar.bse@gmail.com>
2025-11-14 00:54:30 -0300

    Deploy

---
+++ assets/NotFound-DqC6KcsG.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as e,o as c}from"./index-D9RCy4hP.js";const r={};function t(n,s){return c(),e("h1",null,"404")}const f=o(r,[["render",t]]);export{f as default};
